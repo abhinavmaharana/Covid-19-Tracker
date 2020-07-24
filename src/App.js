0300833a226dce6f560ee871a0efa04584092a6e
Focalfossa <abhinavmaharana.004@gmail.com>
2020-07-24 23:54:02 +0530

    Connected Map and info boxes,chart and graph

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React, { useState, useEffect } from 'react';
 import {MenuItem,FormControl,Select, Card, CardContent} from "@material-ui/core";
 import InfoBox from './InfoBox';
 import Map from './Map';
+import Table from "./Table";
 import './App.css';
+import { sortData, prettyPrintStat } from "./util";
+import LineGraph from './LineGraph';
+import "leaflet/dist/leaflet.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country , setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data);
+    });
+  }, []);
 
   // https://disease.sh/v3/covid-19/countries
 
@@ -19,7 +37,10 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2, //UK,USA,FR
           }));
-
+          
+          const sortedData = sortData(data)
+          setTableData(sortedData);
+          setMapCountries(data);
           setCountries(countries);
       });
     };
@@ -28,9 +49,24 @@ function App() {
   }, [countries]);
   
   const onCountryChange = async (event) => {
-    const countryCode = event.target.value;
+    const countryCode= event.target.value;
     setCountry(countryCode);
-  }
+
+    const url = countryCode === 'worldwide' 
+    ? "https://disease.sh/v3/covid-19/all" 
+    : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+   
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode)
+      setCountryInfo(data)
+      setMapCenter([data.countryInfo.lat, data.countryInfo.long])
+      setMapZoom(4);
+    })
+  };
+
+  console.log(countryInfo);
 
   return (
     <div className="app"> 
@@ -50,21 +86,38 @@ function App() {
         </div>
 
         <div className="app__stats">
-          <InfoBox title="Coronavirus Cases" cases={123} total={2000}/>
+          <InfoBox 
+          onClick= {e => setCasesType('cases')}
+            title="Coronavirus Cases" 
+            cases={prettyPrintStat(countryInfo.todayCases)} 
+            total={prettyPrintStat(countryInfo.cases)}/>
 
-          <InfoBox title="Recovered" cases={1234} total={3000}/>
+          <InfoBox 
+           onClick= {e => setCasesType('recovered')}
+            title="Recovered" 
+            cases={prettyPrintStat(countryInfo.todayRecovered)} 
+            total={prettyPrintStat(countryInfo.recovered)} />
 
-          <InfoBox title="Deaths" cases={12345} total={4000}/>
+          <InfoBox 
+           onClick= {e => setCasesType('deaths')}
+            title="Deaths"  
+            cases={prettyPrintStat(countryInfo.todayDeaths)} 
+            total={prettyPrintStat(countryInfo.deaths)} />
         </div>
       
-        <Map />
+        <Map 
+          casesType={casesType} 
+          countries={mapCountries} 
+          center={mapCenter} 
+          zoom={mapZoom} 
+        />
       </div>
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases By Country</h3>
-          {/* Table */}
+          <Table countries={tableData} />
           <h3>Worldwide New Cases</h3>
-          {/* Graph */}
+          <LineGraph />
         </CardContent>
       </Card>
     </div> 
